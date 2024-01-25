f6a1db3842efea1cf88f80506a3c0f6d65cdd519
Denis Holc <holc.denis7@gmail.com>
2024-01-25 18:08:17 +0100

    added process.env.PORT

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 4000;
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const { createServer } = require("node:http");
