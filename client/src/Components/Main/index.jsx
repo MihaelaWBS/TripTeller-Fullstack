d93c99a2ce2b4c0aac35ff7d3a624c00380db74c
Denis Holc <holc.denis7@gmail.com>
2024-01-25 17:07:50 +0100

    run npm run build for dist folder

--- client/src/Components/Main/index.jsx
+++ client/src/Components/Main/index.jsx
@@ -5,19 +5,16 @@ import UpcomingTrips from "../UpcomingTrips";
 import Blog from "../Blog";
 
 const Main = () => {
-	return (
-  
-      <main>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/trips/itinerary" element={<Itinerary />} />
-				<Route path="/trips/upcoming" element={<UpcomingTrips />} />
-                <Route path="/trips/blog" element={<Blog />} />
-			</Routes>
-		</main>
-
-		
-	);
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trips/itinerary" element={<Itinerary />} />
+        <Route path="/trips/upcoming" element={<UpcomingTrips />} />
+        <Route path="/trips/blog" element={<Blog />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default Main;
