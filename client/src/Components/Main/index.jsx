5f7e6fec48828c1b753ad59458c375818e0a877b
MihaelaWBS <mihaela.mocanu.ux@gmail.com>
2024-01-26 17:18:58 +0200

    added logos

--- client/src/Components/Main/index.jsx
+++ client/src/Components/Main/index.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-/* import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Itinerary from "../Itinerary";
 import UpcomingTrips from "../UpcomingTrips";
-import Blog from "../Blog"; */
+import Blog from "../Blog";
+import Home from "../Home";
 
 const Main = () => {
-  return (
-    <></>
-    /*  <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
+	return (
+		<>
+			<main>
+				<Routes>
+        <Route exact path="/" element={<Home />} />	
         <Route path="/trips/itinerary" element={<Itinerary />} />
-        <Route path="/trips/upcoming" element={<UpcomingTrips />} />
-        <Route path="/trips/blog" element={<Blog />} />
-      </Routes>
-    </main> */
-  );
+					<Route path="/trips/upcoming" element={<UpcomingTrips />} />
+					<Route path="/trips/blog" element={<Blog />} />
+				</Routes>
+			</main>
+		</>
+	);
 };
 
 export default Main;
