455d54a64f4db8a77e4fc32a754aa689a4d08142
Denis Holc <holc.denis7@gmail.com>
2024-01-30 17:02:32 +0100

    added RTE

--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -2,14 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import SearchComponent from "../SearchComponent/index";
 import NearbyCities from "../NearbyCities/index";
 import Attractions from "../Attractions/index";
-import TestAddPage from "../TestAddPage";
 
 const index = () => {
   return (
     <>
       <SearchComponent />
       {/*    <NearbyCities /> */}
-      <TestAddPage />
+
       {/*  <Attractions /> */}
     </>
   );
