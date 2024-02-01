d592bfe3012e9662df8dc4454861a74b175594c4
Denis Holc <holc.denis7@gmail.com>
2024-02-01 18:53:12 +0100

    added edit post

--- client/src/Components/PostDescription/index.jsx
+++ client/src/Components/PostDescription/index.jsx
@@ -5,12 +5,14 @@ import parse from "html-react-parser";
 import ReactQuill from "react-quill";
 import DOMPurify from "dompurify";
 import { Link, NavLink, useParams, useNavigate } from "react-router-dom";
+import { Button } from "flowbite-react";
 
 const PostDescription = () => {
   const navigate = useNavigate();
   const [post, setPost] = useState({});
   const { postId } = useParams();
-
+  const [editMode, setEditMode] = useState(false);
+  const [updatedPost, setUpdatedPost] = useState(null);
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -33,6 +35,48 @@ const PostDescription = () => {
     }
   };
 
+  const handleEditClick = () => {
+    setEditMode(true);
+    setUpdatedPost(post);
+  };
+
+  const handleInputChange = async (event) => {
+    const { name, value, files } = event.target;
+
+    if (name === "file") {
+      const file = files[0];
+      try {
+        const imageUrl = await uploadImageToCloudinary(file);
+        setUpdatedPost((prev) => ({ ...prev, picture_url: imageUrl }));
+      } catch (error) {
+        console.error("Failed to upload image", error);
+      }
+    } else if (name === "isHot") {
+      setUpdatedPost((prev) => ({ ...prev, [name]: event.target.checked }));
+    } else {
+      setUpdatedPost((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+  const handleContentChange = (content) => {
+    setUpdatedPost({ ...updatedPost, content });
+  };
+
+  const handleUpdateSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axiosInstance.put(
+        `/api/posts/${postId}`,
+        updatedPost
+      );
+      if (response.status === 200) {
+        setPost(response.data);
+        setEditMode(false);
+      }
+    } catch (error) {
+      console.error("Error updating post:", error);
+    }
+  };
+
   return (
     <>
       {/* Banner with overlay */}
@@ -48,42 +92,57 @@ const PostDescription = () => {
         </div>
       </div>
 
-      {/* Post content area */}
-      {post && (
-        <div className="container mx-auto px-4 py-8">
-          <h2 className="text-3xl font-bold my-4">Title of the Post</h2>
-          <img
-            src="your-post-image-url.jpg" // Replace with your image URL
-            alt="Post"
-            className="w-full h-auto mb-4"
-          />
-          <div>
-            {post.content && typeof post.content === "string" ? (
-              <div className="tailwind-editor-content dark:text-white">
-                {parse(DOMPurify.sanitize(post.content))}
-              </div>
-            ) : (
-              <p>No content available</p>
-            )}
-            {/* <Button onClick={handleEditClick}>Edit Post</Button>
-  <Button onClick={deletePost}>Delete post</Button> */}
-          </div>
-          <div>{post.title}</div>
-
-          {/* Action buttons */}
-          <div className="flex gap-8">
-            <button className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600">
-              Update Post
-            </button>
+      <div className="container mx-auto px-4 py-8">
+        {editMode ? (
+          <>
+            <input
+              type="text"
+              name="title"
+              value={updatedPost.title || ""}
+              onChange={handleInputChange}
+              placeholder="Post Title"
+              className="mb-4 text-xl font-bold"
+            />
+            <ReactQuill
+              theme="snow"
+              value={updatedPost.content || ""}
+              onChange={handleContentChange}
+            />
             <button
-              className="bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600"
-              onClick={handleDelete}
+              className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 mt-4"
+              onClick={handleUpdateSubmit}
             >
-              Delete Post
+              Save Changes
             </button>
-          </div>
-        </div>
-      )}
+          </>
+        ) : (
+          <>
+            <h2 className="text-3xl font-bold my-4">{post.title}</h2>
+            {post.content && (
+              <div
+                className="tailwind-editor-content dark:text-white"
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(post.content),
+                }}
+              />
+            )}
+            <div className="flex gap-8 mt-4">
+              <button
+                className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600"
+                onClick={handleEditClick}
+              >
+                Edit Post
+              </button>
+              <button
+                className="bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600"
+                onClick={handleDelete}
+              >
+                Delete Post
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 };
