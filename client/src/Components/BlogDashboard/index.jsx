d91cf378bb2a38d94822186c375bf82bd5f4e884
Denis Holc <holc.denis7@gmail.com>
2024-02-01 19:22:45 +0100

    added key to blogdashboard

--- client/src/Components/BlogDashboard/index.jsx
+++ client/src/Components/BlogDashboard/index.jsx
@@ -48,44 +48,39 @@ const BlogDashboard = () => {
       {/* Content area for cards */}
       <div className="flex-grow p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts.map((post, userId) => (
-            <Link to={`/blog/posts/${post._id}`}>
-              <div
-                key={userId}
-                className="max-w-sm rounded overflow-hidden shadow-lg bg-white"
-              >
-                <img className="w-full" src={post.imageUrl} alt="Blog post" />
-                <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">{post.title}</div>
-                  <p className="text-gray-700 text-base">{post.content}</p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                  {post.avatar && (
-                    <img
-                      src={post.avatar}
-                      alt="Avatar"
-                      className="w-20 rounded-full"
-                    />
-                  )}
-                  <div className="flex items-center">
-                    <div className="flex flex-col items-center">
-                      <span className="text-sm font-semibold text-gray-700 mr-2 mb-2">
-                        {post.firstName} {post.lastName}{" "}
-                        {/*Here, we would need the user name instead */}
-                      </span>
-                    </div>
-                    {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    {post.date}
-                  </span> */}
+          {posts.map(
+            (
+              post // Removed incorrect usage of `userId` here
+            ) => (
+              <Link to={`/blog/posts/${post._id}`} key={post._id}>
+                {" "}
+                {/* Key is moved here */}
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                  <img className="w-full" src={post.imageUrl} alt="Blog post" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{post.title}</div>
+                    <p className="text-gray-700 text-base">{post.content}</p>
                   </div>
-                  {/*
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-                  {post.readTime}
-                </span> */}
+                  <div className="px-6 pt-4 pb-2">
+                    {post.avatar && (
+                      <img
+                        src={post.avatar}
+                        alt="Avatar"
+                        className="w-20 rounded-full"
+                      />
+                    )}
+                    <div className="flex items-center">
+                      <div className="flex flex-col items-center">
+                        <span className="text-sm font-semibold text-gray-700 mr-2 mb-2">
+                          {post.firstName} {post.lastName}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            )
+          )}
         </div>
       </div>
     </>
