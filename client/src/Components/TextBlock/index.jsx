0173a5bb659bbf5be5c8e33e4c9d7ac7ddb89525
MihaelaWBS <mihaela.mocanu.ux@gmail.com>
2024-02-09 11:13:12 +0100

    added TextBlock

---
+++ client/src/Components/TextBlock/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Blockquote } from 'flowbite-react';
+
+function TextBlock() {
+  return (
+    <Blockquote className="text-center">
+      "Join a world of explorers on TripTeller! Real travelers share experiences that turn into seasoned travel guides. Find your next hidden gem and savor the collective wisdom. Inspired? Read, connect, and contribute your own chapter to our ever-growing travelogue. Your adventure awaits—write and relive with TripTeller! "
+    </Blockquote>
+  );
+}
+
+export default TextBlock;
