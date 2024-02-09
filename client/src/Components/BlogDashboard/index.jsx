60771f2cb6b471775b62e280ceb88973106b28df
Denis Holc <holc.denis7@gmail.com>
2024-02-09 17:47:39 +0100

    fixed width on blog dashboard cards

--- client/src/Components/BlogDashboard/index.jsx
+++ client/src/Components/BlogDashboard/index.jsx
@@ -39,8 +39,8 @@ const BlogDashboard = () => {
       {/* Banner with a cover picture and overlay text */}
       <div className="relative  md:h-[42rem] w-full overflow-hidden">
         <img src={d6} alt="Travel" className="w-full h-full object-fill" />
-        <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-black bg-opacity-10 flex items-center justify-center"> 
-         {/*<h1 className="text-white text-6xl font-bold">Travel smarter</h1> */}
+        <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-black bg-opacity-10 flex items-center justify-center">
+          {/*<h1 className="text-white text-6xl font-bold">Travel smarter</h1> */}
         </div>
       </div>
 
@@ -49,73 +49,73 @@ const BlogDashboard = () => {
         <div className="my-8 text-center ">
           <h2 className="text-4xl font-bold">OUR LATEST POSTS</h2>
         </div>
-     
+
         <div className="text-center mb-10 md:ml-1 xl:ml-10">
           <Link to="/addPost">
             <Button className="bg-orange-500 rounded-3xl">Create a post</Button>
           </Link>
         </div>
-      {/* Content area for cards */}
-      
+        {/* Content area for cards */}
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
           {posts.map(
             (
               post // Removed incorrect usage of `userId` here
             ) => (
-              <Link to={`/blog/posts/${post._id}`} key={post._id} className="max-w-sm mx-auto rounded overflow-hidden shadow-lg bg-white">
+              <Link
+                to={`/blog/posts/${post._id}`}
+                key={post._id}
+                className="max-w-sm w-full mx-auto rounded overflow-hidden shadow-lg bg-white"
+              >
                 {" "}
                 {/* Key is moved here */}
-                
-                  <img
-                    className="h-60 w-full"
-                    src={post.imageUrl}
-                    alt="Blog post"
-                  />
-                  <div className="px-4 py-2">
-                    <div className="font-bold text-xl mb-2">{post.title}</div>
-                  </div>
-                  <div className="w-full border-t-2"></div>
-                  <div className="px-4 py-2 flex justify-between items-center">
-                    {post.avatar && (
+                <img
+                  className="h-60 w-full"
+                  src={post.imageUrl}
+                  alt="Blog post"
+                />
+                <div className="px-4 py-2">
+                  <div className="font-bold text-xl mb-2">{post.title}</div>
+                </div>
+                <div className="w-full border-t-2"></div>
+                <div className="px-4 py-2 flex justify-between items-center">
+                  {post.avatar && (
+                    <img
+                      src={post.avatar}
+                      alt="Avatar"
+                      className="w-20 rounded-full"
+                    />
+                  )}
+                  <div className="flex flex-col">
+                    <div className="flex  items-center gap-6">
                       <img
-                        src={post.avatar}
-                        alt="Avatar"
-                        className="w-20 rounded-full"
+                        src={post.userId?.avatar}
+                        alt="profile-avatar"
+                        className="rounded-3xl"
+                        style={{ width: "40px" }}
                       />
-                    )}
-                    <div className="flex flex-col">
-                     
-                        <div className="flex  items-center gap-6">
-                          <img
-                            src={post.userId?.avatar}
-                            alt="profile-avatar"
-                            className="rounded-3xl"
-                            style={{ width: "40px" }}
-                          />
-                          <div className="flex  flex-col">
-                            <p className="font-bold text-nowrap">
-                              {post.userId &&
-                                `${post.userId.firstName} ${post.userId.lastName}`}
-                            </p>
-                            <div className="flex items-center gap-2">
-                              <FontAwesomeIcon icon={faClock} />
-                              <p>
-                                {new Date(post.createdAt).toLocaleDateString(
-                                  "en-GB",
-                                  {
-                                    day: "2-digit",
-                                    month: "short",
-                                    year: "numeric",
-                                  }
-                                )}
-                              </p>
-                            </div>
-                          </div>
+                      <div className="flex  flex-col">
+                        <p className="font-bold text-nowrap">
+                          {post.userId &&
+                            `${post.userId.firstName} ${post.userId.lastName}`}
+                        </p>
+                        <div className="flex items-center gap-2">
+                          <FontAwesomeIcon icon={faClock} />
+                          <p>
+                            {new Date(post.createdAt).toLocaleDateString(
+                              "en-GB",
+                              {
+                                day: "2-digit",
+                                month: "short",
+                                year: "numeric",
+                              }
+                            )}
+                          </p>
                         </div>
                       </div>
                     </div>
-                  
-                
+                  </div>
+                </div>
               </Link>
             )
           )}
@@ -126,4 +126,3 @@ const BlogDashboard = () => {
 };
 
 export default BlogDashboard;
-
