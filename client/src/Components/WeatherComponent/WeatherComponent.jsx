0c76c7037fa95fbc24ffbae223787af865e8ae46
naeemsajjad1 <you@example.com>
2024-02-12 12:19:36 +0100

    designed weather app

--- client/src/Components/WeatherComponent/WeatherComponent.jsx
+++ client/src/Components/WeatherComponent/WeatherComponent.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import backgroundImage from '../../Images/mountains.webp'
+import backgroundImage from '../../Images/mountains.webp';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThermometerHalf, faThermometerEmpty, faThermometerFull } from '@fortawesome/free-solid-svg-icons';
 
 const WeatherComponent = ({ latitude, longitude, cityName }) => {
   const [dailyForecast, setDailyForecast] = useState(null);
@@ -54,9 +56,10 @@ const WeatherComponent = ({ latitude, longitude, cityName }) => {
           <h2 className="text-2xl font-bold text-center text-white">
             {cityName} - 5-Day Weather Forecast
           </h2>
-          <p className="text-center text-blue-200">
-            Current Temperature: {currentTemperature}°C
-          </p>
+          <div className="text-center text-blue-200">
+            <FontAwesomeIcon icon={faThermometerHalf} /> 
+            <span>Current Temperature: {currentTemperature}°C</span>
+          </div>
         </div>
         <ul className="divide-y divide-blue-800">
           {fiveDayForecast.map((day, index) => (
@@ -65,10 +68,12 @@ const WeatherComponent = ({ latitude, longitude, cityName }) => {
               className="px-4 py-3 flex justify-between items-center bg-white bg-opacity-25"
             >
               <span className="font-medium text-blue-100">{day.date}</span>
-              <span className="text-sm text-blue-200">
-                Max: <span className="font-medium text-white">{day.maxTemp}°C</span>,
-                Min: <span className="font-medium text-white">{day.minTemp}°C</span>
-              </span>
+              <div>
+                <FontAwesomeIcon icon={faThermometerFull} className="text-red-500" /> 
+                <span className="font-medium text-white">{day.maxTemp}°C</span>
+                <FontAwesomeIcon icon={faThermometerEmpty} className="text-blue-500" /> 
+                <span className="font-medium text-white">{day.minTemp}°C</span>
+              </div>
             </li>
           ))}
         </ul>
