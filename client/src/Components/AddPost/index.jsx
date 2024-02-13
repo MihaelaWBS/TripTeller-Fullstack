5628b98112ec959632209b4534944c50ecee15a1
Denis Holc <holc.denis7@gmail.com>
2024-02-13 16:17:56 +0100

    applied dark mode to quill toolbar

--- client/src/Components/AddPost/index.jsx
+++ client/src/Components/AddPost/index.jsx
@@ -3,7 +3,7 @@ import PostForm from "../PostForm";
 
 const index = () => {
   return (
-    <div className="flex max-w-3xl mx-auto ">
+    <div className="flex max-w-3xl mx-auto dark:text-white">
       <PostForm />
     </div>
   );
--- client/src/Components/PostForm/index.jsx
+++ client/src/Components/PostForm/index.jsx
@@ -4,10 +4,9 @@ import QuillEditor from "../RichTextEditor/QuillEditor";
 import parse from "html-react-parser";
 import axiosInstance from "../../axiosInstance";
 import { useNavigate } from "react-router-dom";
-import  TextBlock  from "../TextBlock";
+import TextBlock from "../TextBlock";
 
 import { Button } from "flowbite-react";
-
 
 const PostForm = () => {
   const [editorContent, setEditorContent] = useState("");
@@ -60,74 +59,61 @@ const PostForm = () => {
   };
 
   return (
-   
     <>
-
-    {/* TextBlock component will be the first element and centered on the page */}
-    <div className="flex flex-col mt-40 items-center">
-
-     
+      {/* TextBlock component will be the first element and centered on the page */}
+      <div className="flex flex-col mt-40 items-center">
         <div className="w-full mb-20">
           <TextBlock />
         </div>
-      
+
         {/* <div className="mt-10 w-full flex flex-col items-center "> */}
-          <h1 className="text-center text-4xl font-bold mb-10">ADD YOUR POST</h1>
-          <form onSubmit={handleSubmit} className="flex flex-col items-center gap-3  w-full max-w-xl mx-auto">
-            
-                <input
-                  type="text"
-                  name="title"
-                  value={postData.title}
-                  onChange={handleChange}
-                  placeholder="Title"
-                  // Use the full width within the max-w-xl container
-                  className="w-full p-4 border border-gray-300 rounded-md mb-3"
-                
-                />
-                
-                <input
-                  type="file"
-                  name="picture_url"
-                  onChange={handleChange}
-                  placeholder="Upload Post Image"
-                  // Match the width with the title input within the max-w-xl container
-                  className="w-full p-2.5 border border-gray-300 rounded-md mb-3"
-                  
-                />
+        <h1 className="text-center text-4xl font-bold mb-10 ">ADD YOUR POST</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center gap-3  w-full max-w-xl mx-auto"
+        >
+          <input
+            type="text"
+            name="title"
+            value={postData.title}
+            onChange={handleChange}
+            placeholder="Title"
+            // Use the full width within the max-w-xl container
+            className="w-full p-4 border border-gray-300 rounded-md mb-3 dark:bg-gray-800 dark:placeholder:text-white "
+          />
 
-                
-                <div className="w-full max-w-xl mx-auto">
-                  <QuillEditor
-                    value={editorContent}
-                    onChange={setEditorContent}
-                    handleImageUpload={handleImageUpload}
-                    className="w-full"
-                  />
+          <input
+            type="file"
+            name="picture_url"
+            onChange={handleChange}
+            placeholder="Upload Post Image"
+            // Match the width with the title input within the max-w-xl container
+            className="w-full p-2.5 border border-gray-300 rounded-md mb-3"
+          />
 
-                </div>
-               
-
-           
-
-            <Button type="submit" className="bg-orange-500 rounded-3xl mt-4 px-10 py-2 font-bold">SUBMIT</Button>
-          </form>
-          
-          <div className="tailwind-editor-content mt-5 w-full max-w-xl mx-auto ">
-              <div>{parse(editorContent)}</div>
+          <div className="w-full max-w-xl mx-auto">
+            <QuillEditor
+              value={editorContent}
+              onChange={setEditorContent}
+              handleImageUpload={handleImageUpload}
+              className="w-full"
+            />
           </div>
-          
+
+          <Button
+            type="submit"
+            className="bg-orange-500 rounded-3xl mt-4 px-10 py-2 font-bold"
+          >
+            SUBMIT
+          </Button>
+        </form>
+
+        <div className="tailwind-editor-content mt-5 w-full max-w-xl mx-auto ">
+          <div>{parse(editorContent)}</div>
         </div>
-
+      </div>
     </>
-
-       
-    
   );
 };
 
 export default PostForm;
-
-       
-
-
